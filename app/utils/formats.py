4fd37a55aa7f7a4770eee33bd052c1d776992fb7
Daniel Brotsky <dev@brotsky.com>
2020-06-27 23:28:42 -0700

    Update to handle core field renames (and core fields that don't map to target fields). (#4)
    This involved the following changes:
    
    - Adding a core field map in addition to a custom field map.
    - Always mapping core fields through the field map to get target fields.
    - Understanding address core fields.
    - Adding the capability to do a report of all the AN fields (core and custom)
    - Adding the ability to debug from a local contexts.json file.
    
    Also I moved to using black for auto-formatting, which caused a bunch of whitespace and quote changes.

--- app/utils/formats.py
+++ app/utils/formats.py
@@ -17,15 +17,13 @@ class ANSubmission(HALEasy):
     form_name: str
     body: Any
 
-    def __init__(self,
-                 *args,
-                 id: Optional[int] = 0,
-                 body: Optional[Dict] = None,
-                 **kwargs):
+    def __init__(
+        self, *args, id: Optional[int] = 0, body: Optional[Dict] = None, **kwargs
+    ):
         self.id = id
         self.body = body or {}
-        args = args or ('https://actionnetwork.org',)
-        if kwargs.get('json_str') is None:
+        args = args or ("https://actionnetwork.org",)
+        if kwargs.get("json_str") is None:
             kwargs.update(json_str=json.dumps(body))
         super().__init__(*args, **kwargs)
 
@@ -43,7 +41,7 @@ class ANSubmission(HALEasy):
         items = []
         for d in data:
             for k, v in d.items():
-                if k != 'osdi:submission':
+                if k != "osdi:submission":
                     continue
                 item = cls(id=len(items) + 1, body=v)
                 if form_name := item.get_form_name():
@@ -72,77 +70,111 @@ class ATRecord:
     mod_date: datetime
     core_fields: Dict[str, str]
     custom_fields: Dict[str, Any]
-    record_id: Optional[str] = ''
+    record_id: Optional[str] = ""
     at_match: Optional[ATRecord] = None
 
-    est: ClassVar = gettz('EST')
+    est: ClassVar = gettz("EST")
+    epoch: ClassVar = "1999-01-01 12:00:00 EST"
 
-    core_field_list: ClassVar[List[str]] = [
-        # the core AN fields to update on the AT side
-        # whenever an update is made to the AT side
-        'Email',
-        'First name',
-        'Last name',
-        'Timestamp (EST)',
-    ]
+    an_core_fields: ClassVar[Dict[str, int]] = {}
+    an_custom_fields: ClassVar[Dict[str, int]] = {}
 
     @classmethod
     def from_fields(cls, core_fields: Dict, custom_fields: Dict) -> ATRecord:
-        time_str = core_fields['Timestamp (EST)']
-        time = parse(time_str, tzinfos={'EST': cls.est})
-        return cls(key=core_fields['Email'], mod_date=time,
-                   core_fields=core_fields,
-                   custom_fields=custom_fields)
+        time_str = core_fields["Timestamp (EST)"]
+        time = parse(time_str, tzinfos={"EST": cls.est})
+        return cls(
+            key=core_fields["Email"],
+            mod_date=time,
+            core_fields=core_fields,
+            custom_fields=custom_fields,
+        )
 
     @classmethod
     def from_record(cls, record_data: Dict) -> Optional[ATRecord]:
-        custom_fields = dict(record_data['fields'])
+        custom_fields = dict(record_data["fields"])
+        core_field_map = FC.core_field_map()
+        if not custom_fields.get(core_field_map["Email"]):
+            print(f"Airtable record has no Email field; skipping it: {record_data}")
+            return None
+        if not custom_fields.get(core_field_map["Timestamp (EST)"]):
+            print(f"Airtable record has no Timestamp field; adding it: {record_data}")
+            custom_fields[core_field_map["Timestamp (EST)"]] = cls.epoch
         core_fields = {}
-        for name in cls.core_field_list:
-            if (value := custom_fields.get(name)) is not None:
-                core_fields[name] = value
-                del custom_fields[name]
-            else:
-                print(f"Record schema is missing required "
-                      f"field {name}: {record_data}")
-                return None
-        result = cls.from_fields(core_fields=core_fields,
-                                 custom_fields=custom_fields)
-        result.record_id = record_data['id']
+        for an_name, at_name in core_field_map.items():
+            if (value := custom_fields.get(at_name)) is not None:
+                core_fields[an_name] = value
+                del custom_fields[at_name]
+        result = cls.from_fields(core_fields=core_fields, custom_fields=custom_fields)
+        result.record_id = record_data["id"]
         return result
 
     @classmethod
     def from_submitter(cls, sub_data: Dict[str, Any]) -> ATRecord:
-        addresses = sub_data['email_addresses']
-        address = next((a for a in addresses if a.get('primary')), addresses[0])
-        core_fields = {
-            'Email': address['address'],
-            'First name': sub_data['given_name'],
-            'Last name': sub_data['family_name'],
-            'Timestamp (EST)': cls.convert_to_est(sub_data['modified_date'])
+        for name in sub_data.keys():
+            if name != "custom_fields":
+                cls.an_core_fields[name] = cls.an_core_fields.get(name, 0) + 1
+        emails = sub_data["email_addresses"]
+        email = next((a for a in emails if a.get("primary")), emails[0])
+        addresses = sub_data["postal_addresses"]
+        address = next((a for a in addresses if a.get("primary")), addresses[0])
+        street = address.get("address_lines", [""])[0]
+        phone_fields = {
+            k: v
+            for k, v in sub_data.items()
+            if k.find("hone") >= 0 or k.find("obile") > 0
         }
-        custom_fields: Dict[str, Any] = {}
-        for source_name, source_value in sub_data['custom_fields'].items():
-            target_name = FC.target_field(source_name)
+        if phone_fields:
+            print(f"Found phone fields: {phone_fields}")
+            pass
+        an_core_fields = {
+            "Email": email["address"],
+            "First name": sub_data.get("given_name", ""),
+            "Last name": sub_data.get("family_name", ""),
+            "Address": street,
+            "City": address.get("locality", ""),
+            "State": address.get("region", ""),
+            "Zip Code": address.get("postal_code", ""),
+            "Timestamp (EST)": cls.convert_to_est(sub_data["modified_date"]),
+        }
+        core_fields: Dict[str, str] = {}
+        core_field_map = FC.core_field_map()
+        for an_name, an_value in an_core_fields.items():
+            target_name = core_field_map.get(an_name)
             if target_name:
-                custom_fields[target_name] = source_value
-        return cls.from_fields(core_fields=core_fields,
-                               custom_fields=custom_fields)
+                core_fields[an_name] = an_value
+        custom_fields: Dict[str, Any] = {}
+        for an_name, an_value in sub_data["custom_fields"].items():
+            cls.an_custom_fields[an_name] = cls.an_custom_fields.get(an_name, 0) + 1
+            target_name = FC.target_custom_field(an_name)
+            if target_name:
+                custom_fields[target_name] = an_value
+        return cls.from_fields(core_fields=core_fields, custom_fields=custom_fields)
 
     @classmethod
     def convert_to_est(cls, utc_str: str) -> str:
         utc_time = parse(utc_str)
         est_time = utc_time.astimezone(cls.est)
-        return est_time.strftime('%Y-%m-%d %H:%M:%S %Z')
+        return est_time.strftime("%Y-%m-%d %H:%M:%S %Z")
 
     def add_core_fields(self, updates: Dict[str, Any]):
         # Don't update email since that's the key field
         # used to do the match in the first place.
-        updates.update((name, self.core_fields[name])
-                       for name in self.core_field_list if name != 'Email')
+        core_field_map = FC.core_field_map()
+        updates.update(
+            (at_name, self.core_fields[an_name])
+            for an_name, at_name in core_field_map.items()
+            if an_name != "Email"
+        )
 
     def all_fields(self) -> Dict[str, Any]:
-        return {**self.core_fields, **self.custom_fields}
+        core_field_map = FC.core_field_map()
+        core_fields = {
+            core_field_map[k]: v
+            for k, v in self.core_fields.items()
+            if core_field_map.get(k)
+        }
+        return {**core_fields, **self.custom_fields}
 
     def find_at_field_updates(self) -> Dict[str, Any]:
         """Find fields that are newer on the Action Network side"""
